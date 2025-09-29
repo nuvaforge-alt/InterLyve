ca5cc1a2ab7e77d73289fbcd76e69b6a146e183a
NUVAFORGE.TECH <nuvaforge@gmail.com>
2025-09-29 13:13:06 +0000

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -20,6 +20,7 @@ messaging.onBackgroundMessage(payload => {
   self.registration.showNotification(title, {
     body,
     icon: icon || '/assets/user.png',
+    badge: '/assets/intercon.png', 
     data: data // carry chatId and senderUid
   });
 });
